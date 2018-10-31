875eb719c339fd1d223dd699e26f476be165360d
Nei Barros <joselio.barros@bomcontrole.com.br>
2018-10-31 15:37:41 -0300

    add tarefa routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TarefaRoutes } from './tarefas';
 
 const routes: Routes = [
-
+	{
+		path: '',
+		redirectTo: 'tarefas/listar',
+		pathMatch: 'full'
+	},
+	...TarefaRoutes
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
