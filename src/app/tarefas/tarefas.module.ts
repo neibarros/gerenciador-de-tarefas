5553161924b60dcf20620dbe17f8dbc690290cec
Nei Barros <joselio.barros@bomcontrole.com.br>
2018-10-31 15:06:37 -0300

    add tarefa service

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { TarefaService } from './shared';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+	imports: [
+		CommonModule
+	],
+	declarations: [],
+	providers: [
+		TarefaService
+	]
 })
 export class TarefasModule { }
