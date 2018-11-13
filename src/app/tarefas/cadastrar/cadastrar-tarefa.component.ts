b6075a1ebd7fd17c87d630b816f3a448cb383f87
Josélio A de Barros <p000jbarros@prservicos.com.br>
2018-11-13 14:57:55 -0200

    implementing cadastro de tarefas

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Tarefa, TarefaService } from '../shared';
 
 @Component({
 	selector: 'app-cadastrar-tarefa',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarTarefaComponent implements OnInit {
 
-	constructor() { }
+	@ViewChild('formTarefa') formTarefa: NgForm;
+	tarefa: Tarefa;
+
+	constructor(
+		private tarefaService: TarefaService,
+		private router: Router
+	) { }
 
 	ngOnInit() {
+		this.tarefa = new Tarefa();
+	}
+
+	cadastrar(): void {
+		if (this.formTarefa.form.valid) {
+			this.tarefaService.cadastrar(this.tarefa);
+			this.router.navigate(['/tarefas']);
+		}
 	}
 
 }
