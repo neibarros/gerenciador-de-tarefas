5f4a2a484fe324a33d853eedda0a400d63948f0c
Josélio A de Barros <p000jbarros@prservicos.com.br>
2018-11-13 14:21:08 -0200

    adding route cadastrar

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -17,10 +17,6 @@ export class ListarTarefaComponent implements OnInit {
 
 	ngOnInit() {
 		this.tarefas = this.listarTodos();
-		this.tarefas = [
-			new Tarefa(1, 'Tarefa 1', false),
-			new Tarefa(2, 'Tarefa 2', true)
-		];
 	}
 
 	listarTodos(): Tarefa[] {
