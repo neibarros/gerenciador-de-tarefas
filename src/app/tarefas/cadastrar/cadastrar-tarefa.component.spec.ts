f5635c3a9b869c65708f25d8b315d9605528f3e5
Josélio A de Barros <p000jbarros@prservicos.com.br>
2018-11-13 14:13:09 -0200

    adding cadastro tarefas (indentation)

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.spec.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CadastrarTarefaComponent } from './cadastrar-tarefa.component';
 
 describe('CadastrarTarefaComponent', () => {
-  let component: CadastrarTarefaComponent;
-  let fixture: ComponentFixture<CadastrarTarefaComponent>;
+	let component: CadastrarTarefaComponent;
+	let fixture: ComponentFixture<CadastrarTarefaComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CadastrarTarefaComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ CadastrarTarefaComponent ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CadastrarTarefaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(CadastrarTarefaComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cadastrar-tarefa',
-  templateUrl: './cadastrar-tarefa.component.html',
-  styleUrls: ['./cadastrar-tarefa.component.scss']
+	selector: 'app-cadastrar-tarefa',
+	templateUrl: './cadastrar-tarefa.component.html',
+	styleUrls: ['./cadastrar-tarefa.component.scss']
 })
 export class CadastrarTarefaComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
