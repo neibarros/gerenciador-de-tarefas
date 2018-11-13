5a5ce87de1eabade713bbcf8710d896ba69ccf75
Josélio A de Barros <p000jbarros@prservicos.com.br>
2018-11-13 14:12:07 -0200

    adding cadastro tarefas

---
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-tarefa',
+  templateUrl: './cadastrar-tarefa.component.html',
+  styleUrls: ['./cadastrar-tarefa.component.scss']
+})
+export class CadastrarTarefaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
